4561cfbd0b19c96f7f7c23273d7f4749758509f7
adiluexe <adiluexe@gmail.com>
2024-08-23 17:14:52 +0800

    Feat: Add nextSong function

--- script.js
+++ script.js
@@ -108,7 +108,16 @@ const pauseSong = () => {
   audio.pause();
 };
 
-const playNextSong = () => {};
+const playNextSong = () => {
+  if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  } else {
+    const currentSongIndex = getCurrentSongIndex();
+    const nextSong = userData?.songs[currentSongIndex + 1];
+
+    playSong(nextSong.id);
+  }
+};
 
 const renderSongs = (array) => {
   const songsHTML = array
@@ -144,6 +153,8 @@ playButton.addEventListener("click", () => {
 });
 
 pauseButton.addEventListener("click", pauseSong);
+
+nextButton.addEventListener("click", playNextSong);
 
 const sortSongs = () => {
   userData?.songs.sort((a, b) => {
